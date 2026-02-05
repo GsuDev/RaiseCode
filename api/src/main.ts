b7cdc5302cb2a24948de38ccc6bd4888d3e3efde
Alberto Gómez Serrano <smrgomezal@gmail.com>
2026-02-05 12:03:46 +0100

    feat: Añadido configuración y comprobación de la conexión de SQL

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaSqlService } from './prisma-sql/prisma-sql.service';
+import kleur from 'kleur';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const sql = new PrismaSqlService()
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
+
+  // Mensaje que muestra la url de la api
+  console.log(
+    kleur.green('🌐 ') +
+    kleur.green().bold('Aplicación corriendo en: ') +
+    kleur.cyan(await app.getUrl())
+  );
+
+
+  // Mensaje que muestra el estado con la conexion de SQL
+  try {
+      await sql.$connect()
+      console.log(
+        kleur.green('📁 ') +
+        kleur.green().bold('Se ha conectado a SQL')
+      );
+    } catch (error) {
+      console.error(" Error conectando a SQL", error)
+      console.log(
+        kleur.green('❌ ') +
+        kleur.green().bold('Error conectando a SQL: ') +
+        kleur.cyan(error)
+      );
+    }
+  
 }
 bootstrap();
