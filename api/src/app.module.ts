f98241ec1e2a284b8cccc0c3ffe191c81a354068
Alberto Gómez Serrano <smrgomezal@gmail.com>
2026-02-11 01:27:32 +0100

    Minnor fix

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,6 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { MongooseService } from './mongoose/mongoose.service';
 import { MongooseModule } from './mongoose/mongoose.module';
-import { ConfigModule } from '@nestjs/config';
-
 
 @Module({
   imports: [PrismaModule, MongooseModule],
