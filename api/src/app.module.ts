942d2801aa4d591713d19d32cca53b2b3946b8c9
GsuDev <156043286+GsuDev@users.noreply.github.com>
2026-02-22 19:33:50 +0100

    feat: Añado los nuevos modulos

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { MongooseService } from './mongoose/mongoose.service';
 import { MongooseModule } from './mongoose/mongoose.module';
 import { HealthModule } from './health/health.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-      PrismaModule, 
-      MongooseModule, 
-      HealthModule,
-    ],
+    ConfigModule.forRoot({
+      isGlobal: true, // Carga automáticamente el .env y lo hace disponible en toda la app
+    }),
+    PrismaModule,
+    MongooseModule,
+    HealthModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [PrismaService, MongooseService],
 })
