4714d072a5bfaa7196a18aefc877dad29c44328c
Alberto Gómez Serrano <smrgomezal@gmail.com>
2026-02-24 22:15:20 +0100

    feat: Añadido FeaturedChallenges a HomePage

--- frontend/src/raiseCode/pages/Home/HomePage.tsx
+++ frontend/src/raiseCode/pages/Home/HomePage.tsx
@@ -1,10 +1,13 @@
 import { VStack } from '@chakra-ui/react';
 import { HeroSection } from '../../components/HeroSection/HeroSection';
+import { FeaturedChallenges } from '@/raiseCode/components/FeaturedChallenges/FeaturedChallenges';
+
 export const HomePage = () => {
   return (
     <>
         <VStack justifyContent="center" m={5}>
             <HeroSection />
+            <FeaturedChallenges />
         </VStack>
     </>      
   );
