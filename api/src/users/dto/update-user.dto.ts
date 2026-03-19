a712d7ea3a1ab65ba7900c5aa18aa522144e14f6
carloospg <carlosperezgosalvez@gmail.com>
2026-03-19 22:44:20 +0100

    fix: corregir conflicto de merge en update-user.dto

--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsInt, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsInt, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
@@ -11,23 +11,11 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsOptional()
   lastname?: string;
 
+  @IsEmail({}, { message: 'El email no tiene un formato válido' })
+  @IsOptional()
+  email?: string;
+
   @IsInt({ message: 'El ID del curso debe ser un número entero' })
   @IsOptional()
   courseId?: number;
-}
-import { IsEmail, IsOptional, IsString} from 'class-validator';
-
-// TODO: Completar con los campos que se puedan actualizar
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-    @IsString()
-    @IsOptional()
-    name: string;
-
-    @IsString()
-    @IsOptional()
-    lastname: string;
-
-    @IsEmail({}, { message: 'El email no tiene un formato válido' })
-    @IsOptional()
-    email: string;
 }
