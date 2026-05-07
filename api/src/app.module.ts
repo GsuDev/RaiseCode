4880cd10416eeae4f5685e3e89c091d14babba84
Ian <iaancastellanos1234@gmail.com>
2026-05-07 16:51:25 +0200

    feat: crear ranking.module y registrarlo en app.module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -14,6 +14,7 @@ import { CoursesModule } from './courses/courses.module';
 import { SubjectsModule } from './subjects/subjects.module';
 import { ExecutionModule } from './execution/execution.module';
 import { AchievementsModule } from './achievements/achievements.module';
+import { RankingModule } from './ranking/ranking.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { AchievementsModule } from './achievements/achievements.module';
     SubjectsModule,
     ExecutionModule,
     AchievementsModule,
+    RankingModule,
   ],
   controllers: [],
   providers: [PrismaService, MongooseService],
