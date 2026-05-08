6a2887d1310d1e7e5bebc02ef5bfc8da94cd26d2
carloospg <carlosperezgosalvez@gmail.com>
2026-05-08 02:01:09 +0200

    fix: añado AuthModule al modulo de cursos para resolver dependencia JwtService

--- api/src/courses/courses.module.ts
+++ api/src/courses/courses.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
